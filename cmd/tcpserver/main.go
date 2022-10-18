a3d8bf4026deca2f008f1ba0f913813f6911457d
Artur Opriatnyi <imarrche@gmail.com>
2022-10-18 10:52:34 +0300

    add concurrency

--- cmd/tcpserver/main.go
+++ cmd/tcpserver/main.go
@@ -7,9 +7,9 @@ import (
 	"os"
 )
 
-func main() {
-	l := log.New(os.Stdout, "TCP server: ", 0)
+var l = log.New(os.Stdout, "TCP server: ", 0)
 
+func main() {
 	// getting server's port
 	if len(os.Args) == 1 {
 		l.Fatalln("missing port")
@@ -35,25 +35,30 @@ func main() {
 		// accepting a client
 		c, err := s.Accept()
 		if err != nil {
-			l.Fatalf("client acceptance failed: %v\n", err)
+			l.Printf("client acceptance failed: %v\n", err)
 		}
 
-		// request
-		req, err := bufio.NewReader(c).ReadString('\n')
-		if err != nil {
-			l.Printf("request reading failed: %v\n", err)
-		}
-		l.Printf("request: %s", req)
+		// handling the client concurrently
+		go handle(c)
+	}
+}
 
-		// response
-		if _, err = c.Write([]byte(req)); err != nil {
-			l.Printf("response sending failed: %v\n", err)
-		}
-		l.Printf("response: %s", req)
+func handle(c net.Conn) {
+	// request
+	req, err := bufio.NewReader(c).ReadString('\n')
+	if err != nil {
+		l.Printf("request reading failed: %v\n", err)
+	}
+	l.Printf("request: %s", req)
 
-		// closing client connection
-		if err = c.Close(); err != nil {
-			l.Printf("client connection closing failed: %v\n", err)
-		}
+	// response
+	if _, err = c.Write([]byte(req)); err != nil {
+		l.Printf("response sending failed: %v\n", err)
+	}
+	l.Printf("response: %s", req)
+
+	// closing client connection
+	if err = c.Close(); err != nil {
+		l.Printf("client connection closing failed: %v\n", err)
 	}
 }
