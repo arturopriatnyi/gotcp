52c06954cda2e11a1073c4e648954440bee4ff96
Artur Opriatnyi <imarrche@gmail.com>
2022-10-22 10:35:54 +0300

    change client to send requests in a loop

--- cmd/tcpclient/main.go
+++ cmd/tcpclient/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
 )
 
 func main() {
@@ -16,32 +17,34 @@ func main() {
 	}
 	addr := os.Args[1]
 
-	// connecting to a server
-	c, err := net.Dial("tcp", addr)
-	if err != nil {
-		l.Fatalf("server connection failed: %s\n", addr)
-	}
-	l.Println("client connected")
+	for {
+		// connecting to a server
+		c, err := net.Dial("tcp", addr)
+		if err != nil {
+			l.Fatalf("server connection failed: %s\n", addr)
+		}
+		l.Println("client connected")
 
-	// deferring connection closing
-	defer func(c net.Conn) {
+		// request
+		req := []byte("ping\n")
+		if _, err = c.Write(req); err != nil {
+			l.Printf("request sending failed: %v\n")
+		}
+		l.Printf("request: %s", req)
+
+		// response
+		res, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			l.Printf("response reading failed: %v\n", err)
+		}
+		l.Printf("response: %s", res)
+
+		// closing connection
 		if err = c.Close(); err != nil {
 			l.Fatalf("client connection closing failed: %v\n", err)
 		}
 		l.Println("client connection closed")
-	}(c)
 
-	// request
-	req := []byte("ping\n")
-	if _, err = c.Write(req); err != nil {
-		l.Printf("request sending failed: %v\n")
+		time.Sleep(3 * time.Second)
 	}
-	l.Printf("request: %s", req)
-
-	// response
-	res, err := bufio.NewReader(c).ReadString('\n')
-	if err != nil {
-		l.Printf("response reading failed: %v\n", err)
-	}
-	l.Printf("response: %s", res)
 }
